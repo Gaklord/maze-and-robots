cdc6a7fe805ae427cbd329275cd8d14299c41218
Bladeligermirage <Bladeligermirage@gmail.com>
2011-12-03 20:18:01 +0000

    Mike told me to.

--- Robot Maze Project/src/MemoryRobot.java
+++ Robot Maze Project/src/MemoryRobot.java
@@ -3,11 +3,25 @@ import java.util.Stack;
 
 public  class MemoryRobot extends robot {
 	
+	public MemoryRobot(Maze m)
+	{
+		area = m.findb();
+	}
+	
+	
 	@Override
 	public void Move(Maze m)
 	{
-		 
+		if()
 		
 	}
+	
+	
+	
+	
+	
+	private Stack memory = new Stack();
+	private position area = new position(); 
+
 	
 }
