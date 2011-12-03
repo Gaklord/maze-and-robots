f51685d66bd76b201ef26704394011d08fb001c1
Bladeligermirage@gmail.com <Bladeligermirage@gmail.com@a132819f-b132-82e8-8529-496ecba397eb>
2011-12-03 21:05:09 +0000

    MemoryRobot Updates

--- trunk/Robot Maze Project/src/MemoryRobot.java
+++ trunk/Robot Maze Project/src/MemoryRobot.java
@@ -12,15 +12,51 @@ public  class MemoryRobot extends robot {
 	@Override
 	public void Move(Maze m)
 	{
-		if()
+		while(m.CheckPos(area.getx(), area.gety()) != 3)
+		{
+			if(m.CheckPos(area.getx(), area.gety() + 1) == 1)
+			{
+				memory.push(new position(area.getx(), area.gety()));
+				area.modify(0, 1);
+			}
+			else if(m.CheckPos(area.getx()+1, area.gety()) == 1)
+			{
+				memory.push(new position(area.getx(), area.gety()));
+				area.modify(1, 0);
+			}
+			else if(m.CheckPos(area.getx(), area.gety() -1) == 1)
+			{
+				memory.push(new position(area.getx(), area.gety()));
+				area.modify(0, -1);
+			}
+			else if(m.CheckPos(area.getx()-1, area.gety()) == 1)
+			{
+				if(checkstack() == false)
+				{
+					memory.push(new position(area.getx(), area.gety()));
+					area.modify(-1, 0);
+				}
+			}
+			else
+			{
+				
+			}
+			
 		
+		}
+	}
+	
+	private Boolean checkstack()
+	{
+		if(area == memory.peek())
+		return true;
+		else
+		return false;
 	}
 	
 	
 	
-	
-	
-	private Stack memory = new Stack();
+	private Stack<position> memory;
 	private position area = new position(); 
 
 	
